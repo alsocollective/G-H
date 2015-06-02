09c3f5269b50149cc7f460ca6067052333e950f5
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-06-02 17:38:10 -0400

    added a few files and started with the trillo

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,14 +72,36 @@ app.gloabl = {
 app.product = {
 	mainImage: null,
 	init: function() {
-		app.product.mainImage = $(".left .main img")[0];
 		$(".left .list a").click(app.product.productImageClick);
+		$(".available-colours a").click(app.product.colourClick);
+		$(".sizing button:not(.notavailable)").click(app.product.sizingClick);
+		$(".fitguide").click(app.product.toogleClickGuide);
 	},
 	productImageClick: function(event) {
 		event.preventDefault();
-		app.product.mainImage.src = this.href;
+		$(this.parentNode.parentNode).find(".main img")[0].src = this.href;
+		return false;
+	},
+	colourClick: function(event) {
+		event.preventDefault();
+		$(".multi-product").addClass("hide");
+		$("#" + this.href.split("#").pop()).removeClass("hide");
+		return false;
+	},
+	sizingClick: function(event) {
+		event.preventDefault();
+		var parent = $(this.parentNode.parentNode)
+		parent.find("button.selected").removeClass("selected");
+		$(this).addClass("selected");
+		parent.find(".sizeselector").val(this.id);
+		return false;
+	},
+	toogleClickGuide: function(event) {
+		event.preventDefault();
+		$(".sizechart").toggleClass("hide");
 		return false;
 	}
+
 }
 
 app.index = {
